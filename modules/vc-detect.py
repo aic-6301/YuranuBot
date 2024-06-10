e58f24f473c66747c6aa81316b4b62acf6b8e3cf
YurakunD3suyo <yurakund3sukedo0214@gmail.com>
2024-06-10 12:11:19 +0900

    Fix: 読み上げのぷつぷつ問題を解消

--- modules/vc-detect.py
+++
@@ -1,9 +0,0 @@
-import threading
-import discord
-import modules.settings
-
-def yomiage_watcher():
-    threading.Thread(target=yomiage_watch, args=())
-
-def yomiage_watch():
-    
--- modules/yomiage_main.py
+++ modules/yomiage_main.py
@@ -1,11 +1,7 @@
-import requests
-import threading
 import discord
-import logging
 import platform
-import json
-import wave
 import time
+import wave
 import sys
 import re
 import os
@@ -15,23 +11,20 @@ os_name = platform.uname().system
 from modules.settings import get_server_setting, get_user_setting
 from modules.exception import sendException
 from modules.vc_dictionary import get_dictionary
+from modules.delete import delete_file_latency
 from collections import deque, defaultdict
 from discord import FFmpegOpusAudio
-from modules.delete import delete_file_latency
-if os_name == "Linux":
-    import voicevox_core
-    from voicevox_core import AccelerationMode, AudioQuery, VoicevoxCore
+
+from voicevox_core import AccelerationMode, AudioQuery, VoicevoxCore
 
 ###読み上げ用のコアをロードし、作成します
-    core = VoicevoxCore(
-        acceleration_mode=AccelerationMode.AUTO,
-        open_jtalk_dict_dir = './voicevox/open_jtalk_dic_utf_8-1.11'
-    )
+core = VoicevoxCore(
+    acceleration_mode=AccelerationMode.AUTO,
+    open_jtalk_dict_dir = './voicevox/open_jtalk_dic_utf_8-1.11'
+)
 
 ## VOICEVOX用の設定
 VC_OUTPUT = "./yomiage_data/"
-VC_HOST = "127.0.0.1"
-VC_PORT = 50021
 FS = 24000
 
 yomiage_serv_list = defaultdict(deque)
@@ -101,60 +94,34 @@ async def yomiage(content, guild: discord.Guild):
 
 async def queue_yomiage(content: str, guild: discord.Guild, spkID: int):    
     try:
-        if os_name == "Windows":
-            speed = get_server_setting(guild.id, "speak_speed")
-            # 音声化する文言と話者を指定(3で標準ずんだもんになる)
-            params = (
-                ('text', content),
-                ('speaker', spkID)
-            )
-            _query = requests.post(
-                f'http://{VC_HOST}:{VC_PORT}/audio_query',
-                params=params
-            )
-            query = _query.json()
-            query["speedScale"] = speed
+    ##サーバーごとに利用される速度のデータを取得
+        speed = get_server_setting(guild.id, "speak_speed")
 
-            synthesis = requests.post(
-                f'http://{VC_HOST}:{VC_PORT}/synthesis',
-                headers = {"Content-Type": "application/json"},
-                params = params,
-                data = json.dumps(query)
-            )
-            voice = synthesis.content
+        core.load_model(spkID)
 
-        elif os_name == "Linux":
-        ##サーバーごとに利用される速度のデータを取得
-            speed = get_server_setting(guild.id, "speak_speed")
-
-            core.load_model(spkID)
-
-            audio_query = core.audio_query(content, spkID)
-            audio_query.speed_scale = speed
-            
-            voice = core.synthesis(audio_query, spkID)
+        audio_query = core.audio_query(content, spkID)
+        audio_query.speed_scale = speed
         
+        voice_byte = core.synthesis(audio_query, spkID)
+    
         ###作成時間を記録するため、timeを利用する
         wav_time = time.time()
         voice_file = f"{VC_OUTPUT}{guild.id}-{wav_time}.wav"
 
-        with wave.open(voice_file, "w") as wf:
-            wf.setnchannels(1)  # チャンネル数の設定 (1:mono, 2:stereo)
-            wf.setsampwidth(2)
-            wf.setframerate(FS) 
-            wf.writeframes(voice)  # ステレオデータを書きこむ
+        with open(voice_file, "wb") as f:
+            f.write(voice_byte)
 
-        with wave.open(voice_file,  'rb') as wr:\
+        with wave.open(voice_file,  'rb') as f:
             # 情報取得
-            fr = wr.getframerate()
-            fn = wr.getnframes()
+            fr = f.getframerate()
+            fn = f.getnframes()
             length = fn / fr
 
         file_list = [voice_file, length]
 
         queue = yomiage_serv_list[guild.id]
         queue.append(file_list)
-    
+
         if not guild.voice_client.is_playing():
             send_voice(queue, guild.voice_client)
         return
