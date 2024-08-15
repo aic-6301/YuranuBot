8d78cceabf517b9ba821292a650c80669cf11a90
YurakunD3suyo <yurakund3sukedo0214@gmail.com>
2024-08-16 03:12:23 +0900

    fix

--- modules/yomiage_main.py
+++ modules/yomiage_main.py
@@ -57,6 +57,7 @@ sound_effects = [
     ["自然係のPC", "explosion.mp3", 0.15, "https://tenor.com/view/house-explosion-explode-boom-kaboom-gif-19506150"],
     ["どっかぁん", "explosion.mp3", 0.15, "https://tenor.com/view/house-explosion-explode-boom-kaboom-gif-19506150"],
     ["サ終", "explosion.mp3", 0.15, "https://tenor.com/view/house-explosion-explode-boom-kaboom-gif-19506150"],
+    ["心折れた", "explosion.mp3", 0.15, "https://tenor.com/view/house-explosion-explode-boom-kaboom-gif-19506150"],
 
     ["まだだめだ", "madadameda.mp3", 0.2, None],
     ["ばばんばばん", "ace.mp3", 0.2, None],
