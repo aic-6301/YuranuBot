de9c160d330c7aaaa1cc5963b93b72d05291cd50
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2024-08-22 11:57:18 +0900

    Update modules/exception.py
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- modules/exception.py
+++ modules/exception.py
@@ -13,7 +13,7 @@ async def sendException(e, filename, line_no):
     embed = discord.Embed(  # Embedを定義する
         title="うまくいかなかったのだ。",  # タイトル
         color=discord.Color.red(),  # フレーム色指定(ぬーん神)
-        description=f"例外エラーが発生しました！詳細はこちらをご覧ください。",  # Embedの説明文 必要に応じて
+        description="例外エラーが発生しました！詳細はこちらをご覧ください。",  # Embedの説明文 必要に応じて
     )
     embed.add_field(
         name="**//エラー内容//**",
